51476ef689f5936145575f1a07fd5ee2a34d1a45
KRANTHI AKURATHI <akurathikranthi12@gmail.com>
2026-06-01 23:01:44 +0530

    Update HomePage.jsx

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from 'react-router-dom';
 function HomePage() {
   const [products, setProducts] = useState([]);
   const navigate = useNavigate();
-
+//
   useEffect(() => {
     const userID = localStorage.getItem('userID');
     if (!userID) {
